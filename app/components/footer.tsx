82993254e6a925ee78a8e91c85dcb629e1eea96d
Riad Zaid <62952229+RiadZX@users.noreply.github.com>
2025-02-22 22:15:39 +0100

    Added Source

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/riadzx"
+            href="https://github.com/RiadZX/portfolio"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
